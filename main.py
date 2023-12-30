c20d6a7314f5fd5f96409e8ee8f4e045e7003731
Javad Estaji <applets_menisci_0c@icloud.com>
2023-12-30 20:46:45 +0100

    Added YOLO implementation

--- main.py
+++ main.py
@@ -1,9 +1,55 @@
-import pyrealsense2 as rs
-import numpy as np
+from ultralytics import YOLO
 import cv2
-import tensorflow as tf
+import random
+import numpy as np
+import pyrealsense2 as rs
 
-# Configure depth and color streams
+
+def overlay(image, mask, color, alpha, resize=None):
+    """Combines image and its segmentation mask into a single image.
+
+    Params:
+        image: Training image. np.ndarray,
+        mask: Segmentation mask. np.ndarray,
+        color: Color for segmentation mask rendering.  tuple[int, int, int] = (255, 0, 0)
+        alpha: Segmentation mask's transparency. float = 0.5,
+        resize: If provided, both image and its mask are resized before blending them together.
+        tuple[int, int] = (1024, 1024))
+
+    Returns:
+        image_combined: The combined image. np.ndarray
+
+    """
+    # color = color[::-1]
+    colored_mask = np.expand_dims(mask, 0).repeat(3, axis=0)
+    colored_mask = np.moveaxis(colored_mask, 0, -1)
+    masked = np.ma.MaskedArray(image, mask=colored_mask, fill_value=color)
+    image_overlay = masked.filled()
+
+    if resize is not None:
+        image = cv2.resize(image.transpose(1, 2, 0), resize)
+        image_overlay = cv2.resize(image_overlay.transpose(1, 2, 0), resize)
+
+    image_combined = cv2.addWeighted(image, 1 - alpha, image_overlay, alpha, 0)
+
+    return image_combined
+
+
+def plot_one_box(x, img, color=None, label=None, line_thickness=3):
+    # Plots one bounding box on image img
+    tl = line_thickness or round(0.002 * (img.shape[0] + img.shape[1]) / 2) + 1  # line/font thickness
+    color = color or [random.randint(0, 255) for _ in range(3)]
+    c1, c2 = (int(x[0]), int(x[1])), (int(x[2]), int(x[3]))
+    cv2.rectangle(img, c1, c2, color, thickness=tl, lineType=cv2.LINE_AA)
+    if label:
+        tf = max(tl - 1, 1)  # font thickness
+        t_size = cv2.getTextSize(label, 0, fontScale=tl / 3, thickness=tf)[0]
+        c2 = c1[0] + t_size[0], c1[1] - t_size[1] - 3
+        cv2.rectangle(img, c1, c2, color, -1, cv2.LINE_AA)  # filled
+        cv2.putText(img, label, (c1[0], c1[1] - 2), 0, tl / 3, [225, 255, 255], thickness=tf, lineType=cv2.LINE_AA)
+
+
+# Initialize RealSense camera
 pipeline = rs.pipeline()
 config = rs.config()
 config.enable_stream(rs.stream.color, 1280, 720, rs.format.bgr8, 30)
@@ -12,76 +58,45 @@ print("[INFO] Starting streaming...")
 pipeline.start(config)
 print("[INFO] Camera ready.")
 
-# download model from: https://github.com/opencv/opencv/wiki/TensorFlow-Object-Detection-API#run-network-in-opencv
-print("[INFO] Loading model...")
-PATH_TO_CKPT = "frozen_inference_graph.pb"
+# Load a model
+model = YOLO("yolov8m-seg.pt")
+class_names = model.names
+print('Class Names: ', class_names)
+colors = [[random.randint(0, 255) for _ in range(3)] for _ in class_names]
 
-# Load the Tensorflow model into memory.
-detection_graph = tf.Graph()
-with detection_graph.as_default():
-    od_graph_def = tf.compat.v1.GraphDef()
-    with tf.compat.v1.gfile.GFile(PATH_TO_CKPT, 'rb') as fid:
-        serialized_graph = fid.read()
-        od_graph_def.ParseFromString(serialized_graph)
-        tf.compat.v1.import_graph_def(od_graph_def, name='')
-    sess = tf.compat.v1.Session(graph=detection_graph)
-
-# Input tensor is the image
-image_tensor = detection_graph.get_tensor_by_name('image_tensor:0')
-# Output tensors are the detection boxes, scores, and classes
-# Each box represents a part of the image where a particular object was detected
-detection_boxes = detection_graph.get_tensor_by_name('detection_boxes:0')
-# Each score represents level of confidence for each of the objects.
-# The score is shown on the result image, together with the class label.
-detection_scores = detection_graph.get_tensor_by_name('detection_scores:0')
-detection_classes = detection_graph.get_tensor_by_name('detection_classes:0')
-# Number of objects detected
-num_detections = detection_graph.get_tensor_by_name('num_detections:0')
-# code source of tensorflow model loading:
-# https://www.geeksforgeeks.org/ml-training-image-classifier-using-tensorflow-object-detection-api/
-print("[INFO] Model loaded.")
-colors_hash = {}
 while True:
     frames = pipeline.wait_for_frames()
     color_frame = frames.get_color_frame()
+    if not color_frame:
+        continue
 
-    # Convert images to numpy arrays
     color_image = np.asanyarray(color_frame.get_data())
-    scaled_size = (color_frame.width, color_frame.height)
-    # expand image dimensions to have shape: [1, None, None, 3]
-    # i.e. a single-column array, where each item in the column has the pixel RGB value
-    image_expanded = np.expand_dims(color_image, axis=0)
-    # Perform the actual detection by running the model with the image as input
-    (boxes, scores, classes, num) = sess.run([detection_boxes, detection_scores, detection_classes, num_detections],
-                                             feed_dict={image_tensor: image_expanded})
 
-    boxes = np.squeeze(boxes)
-    classes = np.squeeze(classes).astype(np.int32)
-    scores = np.squeeze(scores)
+    h, w, _ = color_image.shape
+    results = model.predict(color_image, stream=True)
+    # print(results)
+    for r in results:
+        boxes = r.boxes  # Boxes object for bbox outputs
+        masks = r.masks  # Masks object for segment masks outputs
+        probs = r.probs  # Class probabilities for classification outputs
 
-    for idx in range(int(num)):
-        class_ = classes[idx]
-        score = scores[idx]
-        box = boxes[idx]
+    if masks is not None:
+        masks = masks.data.cpu()
+        for seg, box in zip(masks.data.cpu().numpy(), boxes):
+            seg = cv2.resize(seg, (w, h))
+            color_image = overlay(color_image, seg, colors[int(box.cls)], 0.4)
 
-        if class_ not in colors_hash:
-            colors_hash[class_] = tuple(np.random.choice(range(256), size=3))
+            xmin = int(box.data[0][0])
+            ymin = int(box.data[0][1])
+            xmax = int(box.data[0][2])
+            ymax = int(box.data[0][3])
 
-        if score > 0.6:
-            left = int(box[1] * color_frame.width)
-            top = int(box[0] * color_frame.height)
-            right = int(box[3] * color_frame.width)
-            bottom = int(box[2] * color_frame.height)
+            plot_one_box([xmin, ymin, xmax, ymax], color_image, colors[int(box.cls)],
+                         f'{class_names[int(box.cls)]} {float(box.conf):.3}')
 
-            p1 = (left, top)
-            p2 = (right, bottom)
-            # draw box
-            r, g, b = colors_hash[class_]
-            cv2.rectangle(color_image, p1, p2, (int(r), int(g), int(b)), 2, 1)
-
-    cv2.namedWindow('RealSense', cv2.WINDOW_AUTOSIZE)
-    cv2.imshow('RealSense', color_image)
-    cv2.waitKey(1)
+    cv2.imshow('img', color_image)
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
 
 print("[INFO] stop streaming ...")
 pipeline.stop()
